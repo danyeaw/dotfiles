3cf5e18eade6a909a50c5e6d7b111f3c3a6dabbd
Dan Yeaw <dan@yeaw.me>
2021-11-26 16:31:02 -0500

    Set fish as default shell

---
+++ config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+
