95699b49b4893085002a5ca0dc4645c12a787810
Dan Yeaw <dan@yeaw.me>
2023-06-24 17:00:02 -0400

    Add Google Cloud SDK and rbenv

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,8 @@ end
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
 
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/dan/google-cloud-sdk/path.fish.inc' ]; . '/home/dan/google-cloud-sdk/path.fish.inc'; end
+# rbenv
+status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source
