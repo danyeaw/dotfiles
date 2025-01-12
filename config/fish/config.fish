96a05cd102c2104a302022bed6c5ea8530f78d89
Dan Yeaw <dan@yeaw.me>
2025-01-12 09:01:30 -0500

    Fix pyenv init

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,13 +1,13 @@
-if test -f ~/.local/bin/pyenv
-    status is-login; and pyenv init --path | source
-    status is-interactive; and pyenv init - | source
+# pyenv
+if test -f ~/.pyenv/bin/pyenv
+    pyenv init - fish | source
 end
-
 
 # The next line updates PATH for the Google Cloud SDK.
 if test -f ~/google-cloud-sdk/path.fish.inc
     . '~/google-cloud-sdk/path.fish.inc'
 end
+
 # rbenv
 if test -f ~/.rbenv/bin/rbenv
     status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source
